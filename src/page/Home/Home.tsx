10dfd8b435cb3df992be1befada391ef82df4160
Timothée VANNIER <tva@subteno.com>
2023-05-11 11:45:11 +0200

    [FIX] wrong charts sizes

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -15,18 +15,16 @@ export default function Home() {
 
 
 	return (
-		<div className={"pl-[100px] pr-[90px] py-[70px] w-full"}>
+		<div className={"pl-[100px] pr-[90px] py-[70px] pb-0 w-full"}>
 			<h1 className={"text-[48px]"}>Bonjour <span className={"text-secondary"}>{data.userInfos.firstName}</span>
 			</h1>
 			<h2 className={"text-lg font-[400] mt-[40px]"}>Félicitation ! Vous avez explosé vos objectifs hier 👏</h2>
-			<div className={"grid grid-rows-[320px_260px] gap-[30px] w-full h-full mt-[70px]"}
-			style={{gridTemplateColumns: "835px 260px"}}
-			>
+			<div className={"grid grid-rows-[320px_260px] grid-cols-[835px_260px] gap-[30px] w-full mt-[70px]"}>
 				<Activities/>
 				<div className={"row-span-2 h-full grid grid-rows-4 gap-4"}>
 					<KeyDatas data={data.keyData}/>
 				</div>
-				<div className={"grid grid-cols-3 gap-4"}>
+				<div className={"grid grid-cols-[repeat(3,_260px)] gap-4"}>
 					<MedianSessions/>
 					<div className={"border-2"}>7</div>
 					<div className={"border-2"}>8</div>
