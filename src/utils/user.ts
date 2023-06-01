c686940b49abaeb88f859b1423429fc8e79f3a3e
Timothée VANNIER <tva@subteno.com>
2023-06-01 12:43:54 +0200

    [FIX] useless console.log

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -18,7 +18,6 @@ export class CheckUserData {
 	constructor({id, userInfos, todayScore, keyData}: UserMainData) {
 		this.id = id;
 		this.userInfos = userInfos;
-		console.log(todayScore);
 		this.todayScore = todayScore*100;
 		this.keyData = {
 			calorieCount: `${(keyData.calorieCount/1000).toFixed(3)}kCal`,
